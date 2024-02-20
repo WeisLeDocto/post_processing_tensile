91d969a92e9d36a37460076e8e678cbc5b06a4e8
Antoine Weisrock <antoine.weisrock@gmail.com>
2024-02-20 12:11:12 +0100

    refactor: update processing parameters

--- parameters/nb_pts_begin_end.mk
+++ parameters/nb_pts_begin_end.mk
@@ -1,3 +1,3 @@
 # The number of points to use for the Savitzky–Golay filter applied when
 # determining the end extension of the valid data
-NB_POINTS_SMOOTH_END := 4000
+NB_POINTS_SMOOTH_END := 3000
--- parameters/nb_pts_smooth.mk
+++ parameters/nb_pts_smooth.mk
@@ -1,3 +1,3 @@
 # The number of points to use for the Savitzky–Golay filter applied when
 # smoothening the raw experimental data
-NB_POINTS_SMOOTH := 50
+NB_POINTS_SMOOTH := 400
