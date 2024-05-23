7fc3094774f0158e08886e0a1bfd3ebc1972ccf2
Antoine Weisrock <antoine.weisrock@gmail.com>
2024-05-23 14:53:36 +0200

    chore: update default PEAK_RANGE to 10000 points

--- parameters/peak_thresh.mk
+++ parameters/peak_thresh.mk
@@ -2,4 +2,4 @@
 # will be PEAK_THRESHOLD / 100 * (max_stress - min_stress)
 # The maximum width of the peak, as a number of samples, will be PEAK_RANGE
 export PEAK_THRESHOLD := 1
-export PEAK_RANGE := 1000
+export PEAK_RANGE := 10000
